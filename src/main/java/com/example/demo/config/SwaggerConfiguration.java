2f3b626f4417363a913c50f29df79eaca14ed839
gennzee <tanks.museum98@gmail.com>
2021-04-07 00:06:18 +0700

    06/04/2021 : change Swagger v3 -> v2. Because v3 not supporting JWT Bearer authorization.

--- src/main/java/com/example/demo/config/SwaggerConfiguration.java
+++ src/main/java/com/example/demo/config/SwaggerConfiguration.java
@@ -32,7 +32,7 @@ public class SwaggerConfiguration {
 
     @Bean
     public Docket api() {
-        return new Docket(DocumentationType.OAS_30)
+        return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
                 .securityContexts(Arrays.asList(securityContext()))
                 .securitySchemes(Arrays.asList(apiKey()))
